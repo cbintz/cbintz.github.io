b859ea5558e87e0b2e3e6c4addbacc9f5ea96e3a
Corinne Bintz <cbintz@middlebury.edu>
2020-09-06 09:55:59 -0400

    added new pics

--- append_image.js
+++ append_image.js
@@ -125,7 +125,7 @@ function add_image4(parent, width, height){
 
 
     var myimage = chart.append('image')
-    .attr('xlink:href', 'escience.jpeg')
+    .attr('xlink:href', 'aasd.png')
     .attr('width', 500)
     .attr('height', 500)
     .attr("x", 0)
@@ -163,7 +163,7 @@ function add_image5(parent, width, height){
 
 
     var myimage = chart.append('image')
-    .attr('xlink:href', 'mystrength-logo.png')
+    .attr('xlink:href', 'opioid.png')
     .attr('width', 500)
     .attr('height', 500)
     .attr("x", 0)
@@ -187,6 +187,7 @@ function add_image5(parent, width, height){
     return update_chart;
 
 }
+
 function add_image6(parent, width, height){
     const margins = {top:10, bottom:50, left:50, right:10};
     const chart_width = width - margins.left - margins.right;
@@ -224,6 +225,7 @@ function add_image6(parent, width, height){
     return update_chart;
 
 }
+
 function add_image7(parent, width, height){
     const margins = {top:10, bottom:50, left:50, right:10};
     const chart_width = width - margins.left - margins.right;
@@ -237,7 +239,7 @@ function add_image7(parent, width, height){
 
 
     var myimage = chart.append('image')
-    .attr('xlink:href', 'middlebury.jpeg')
+    .attr('xlink:href', 'escience.jpeg')
     .attr('width', 500)
     .attr('height', 500)
     .attr("x", 0)
@@ -261,9 +263,6 @@ function add_image7(parent, width, height){
     return update_chart;
 
 }
-
-
-
 
 
 function add_image8(parent, width, height){
@@ -279,7 +278,7 @@ function add_image8(parent, width, height){
 
 
     var myimage = chart.append('image')
-    .attr('xlink:href', 'middlebury.jpeg')
+    .attr('xlink:href', 'DIS.png')
     .attr('width', 500)
     .attr('height', 500)
     .attr("x", 0)
@@ -303,6 +302,7 @@ function add_image8(parent, width, height){
     return update_chart;
 
 }
+
 function add_image9(parent, width, height){
     const margins = {top:10, bottom:50, left:50, right:10};
     const chart_width = width - margins.left - margins.right;
@@ -316,7 +316,7 @@ function add_image9(parent, width, height){
 
 
     var myimage = chart.append('image')
-    .attr('xlink:href', 'girls_inc_pnw.jpg')
+    .attr('xlink:href', 'HRAP.jpg')
     .attr('width', 500)
     .attr('height', 500)
     .attr("x", 0)
@@ -340,6 +340,7 @@ function add_image9(parent, width, height){
     return update_chart;
 
 }
+
 function add_image10(parent, width, height){
     const margins = {top:10, bottom:50, left:50, right:10};
     const chart_width = width - margins.left - margins.right;
@@ -353,7 +354,7 @@ function add_image10(parent, width, height){
 
 
     var myimage = chart.append('image')
-    .attr('xlink:href', 'images.jpeg')
+    .attr('xlink:href', 'mystrength-logo.png')
     .attr('width', 500)
     .attr('height', 500)
     .attr("x", 0)
@@ -390,7 +391,7 @@ function add_image11(parent, width, height){
 
 
     var myimage = chart.append('image')
-    .attr('xlink:href', 'gears.jpg')
+    .attr('xlink:href', 'middlebury.jpeg')
     .attr('width', 500)
     .attr('height', 500)
     .attr("x", 0)
@@ -427,7 +428,7 @@ function add_image12(parent, width, height){
 
 
     var myimage = chart.append('image')
-    .attr('xlink:href', 'people.jpg')
+    .attr('xlink:href', 'middlebury.jpeg')
     .attr('width', 500)
     .attr('height', 500)
     .attr("x", 0)
@@ -451,6 +452,11 @@ function add_image12(parent, width, height){
     return update_chart;
 
 }
+
+
+
+
+
 function add_image13(parent, width, height){
     const margins = {top:10, bottom:50, left:50, right:10};
     const chart_width = width - margins.left - margins.right;
@@ -464,7 +470,7 @@ function add_image13(parent, width, height){
 
 
     var myimage = chart.append('image')
-    .attr('xlink:href', 'grace_hopper.png')
+    .attr('xlink:href', 'middlebury.jpeg')
     .attr('width', 500)
     .attr('height', 500)
     .attr("x", 0)
@@ -501,7 +507,7 @@ function add_image14(parent, width, height){
 
 
     var myimage = chart.append('image')
-    .attr('xlink:href', 'grad.png')
+    .attr('xlink:href', 'girls_inc_pnw.jpg')
     .attr('width', 500)
     .attr('height', 500)
     .attr("x", 0)
@@ -538,7 +544,7 @@ function add_image15(parent, width, height){
 
 
     var myimage = chart.append('image')
-    .attr('xlink:href', 'alpine.jpeg')
+    .attr('xlink:href', 'images.jpeg')
     .attr('width', 500)
     .attr('height', 500)
     .attr("x", 0)
@@ -563,6 +569,191 @@ function add_image15(parent, width, height){
 
 }
 function add_image16(parent, width, height){
+    const margins = {top:10, bottom:50, left:50, right:10};
+    const chart_width = width - margins.left - margins.right;
+    const chart_height = height - margins.top - margins.bottom;
+
+    let opacity = 1;
+
+    const chart = parent.append("g")
+    .attr("id", "image1")
+    .style("opacity", 0)
+
+
+    var myimage = chart.append('image')
+    .attr('xlink:href', 'gears.jpg')
+    .attr('width', 500)
+    .attr('height', 500)
+    .attr("x", 0)
+
+    const update_chart = function(){
+        console.log("bar chart opacity", opacity);
+        chart.transition()
+        .duration(800)
+        .style("opacity", opacity);
+    }
+
+    update_chart.opacity = (new_opacity)=>{
+        if (new_opacity !==undefined){
+            opacity = new_opacity;
+            return update_chart;
+        }else{
+            return opacity;
+        }
+
+    }
+    return update_chart;
+
+}
+function add_image17(parent, width, height){
+    const margins = {top:10, bottom:50, left:50, right:10};
+    const chart_width = width - margins.left - margins.right;
+    const chart_height = height - margins.top - margins.bottom;
+
+    let opacity = 1;
+
+    const chart = parent.append("g")
+    .attr("id", "image1")
+    .style("opacity", 0)
+
+
+    var myimage = chart.append('image')
+    .attr('xlink:href', 'people.jpg')
+    .attr('width', 500)
+    .attr('height', 500)
+    .attr("x", 0)
+
+    const update_chart = function(){
+        console.log("bar chart opacity", opacity);
+        chart.transition()
+        .duration(800)
+        .style("opacity", opacity);
+    }
+
+    update_chart.opacity = (new_opacity)=>{
+        if (new_opacity !==undefined){
+            opacity = new_opacity;
+            return update_chart;
+        }else{
+            return opacity;
+        }
+
+    }
+    return update_chart;
+
+}
+function add_image18(parent, width, height){
+    const margins = {top:10, bottom:50, left:50, right:10};
+    const chart_width = width - margins.left - margins.right;
+    const chart_height = height - margins.top - margins.bottom;
+
+    let opacity = 1;
+
+    const chart = parent.append("g")
+    .attr("id", "image1")
+    .style("opacity", 0)
+
+
+    var myimage = chart.append('image')
+    .attr('xlink:href', 'grace_hopper.png')
+    .attr('width', 500)
+    .attr('height', 500)
+    .attr("x", 0)
+
+    const update_chart = function(){
+        console.log("bar chart opacity", opacity);
+        chart.transition()
+        .duration(800)
+        .style("opacity", opacity);
+    }
+
+    update_chart.opacity = (new_opacity)=>{
+        if (new_opacity !==undefined){
+            opacity = new_opacity;
+            return update_chart;
+        }else{
+            return opacity;
+        }
+
+    }
+    return update_chart;
+
+}
+function add_image19(parent, width, height){
+    const margins = {top:10, bottom:50, left:50, right:10};
+    const chart_width = width - margins.left - margins.right;
+    const chart_height = height - margins.top - margins.bottom;
+
+    let opacity = 1;
+
+    const chart = parent.append("g")
+    .attr("id", "image1")
+    .style("opacity", 0)
+
+
+    var myimage = chart.append('image')
+    .attr('xlink:href', 'grad.png')
+    .attr('width', 500)
+    .attr('height', 500)
+    .attr("x", 0)
+
+    const update_chart = function(){
+        console.log("bar chart opacity", opacity);
+        chart.transition()
+        .duration(800)
+        .style("opacity", opacity);
+    }
+
+    update_chart.opacity = (new_opacity)=>{
+        if (new_opacity !==undefined){
+            opacity = new_opacity;
+            return update_chart;
+        }else{
+            return opacity;
+        }
+
+    }
+    return update_chart;
+
+}
+function add_image20(parent, width, height){
+    const margins = {top:10, bottom:50, left:50, right:10};
+    const chart_width = width - margins.left - margins.right;
+    const chart_height = height - margins.top - margins.bottom;
+
+    let opacity = 1;
+
+    const chart = parent.append("g")
+    .attr("id", "image1")
+    .style("opacity", 0)
+
+
+    var myimage = chart.append('image')
+    .attr('xlink:href', 'alpine.jpeg')
+    .attr('width', 500)
+    .attr('height', 500)
+    .attr("x", 0)
+
+    const update_chart = function(){
+        console.log("bar chart opacity", opacity);
+        chart.transition()
+        .duration(800)
+        .style("opacity", opacity);
+    }
+
+    update_chart.opacity = (new_opacity)=>{
+        if (new_opacity !==undefined){
+            opacity = new_opacity;
+            return update_chart;
+        }else{
+            return opacity;
+        }
+
+    }
+    return update_chart;
+
+}
+function add_image21(parent, width, height){
     const margins = {top:10, bottom:50, left:50, right:10};
     const chart_width = width - margins.left - margins.right;
     const chart_height = height - margins.top - margins.bottom;
